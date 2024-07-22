1bf321575184419198b5f59226636dd7f1123390
Alexei Myshkouski <alexeimyshkouski@gmail.com>
2024-07-22 11:49:09 +0300

    wip

--- src/commonMain/kotlin/io/github/myshkouski/kotlin/engine/Engine.kt
+++ src/commonMain/kotlin/io/github/myshkouski/kotlin/engine/Engine.kt
@@ -1,6 +1,8 @@
 package io.github.myshkouski.kotlin.engine
 
 import io.github.myshkouski.kotlin.fact.Fact
+import io.github.myshkouski.kotlin.fact.ValueProvider
+import io.github.myshkouski.kotlin.rule.Rule
 import io.github.myshkouski.kotlin.rule.RuleContext
 import io.github.myshkouski.kotlin.storage.Storage
 import kotlin.js.ExperimentalJsExport
@@ -42,4 +44,79 @@ internal class DefaultEngine(
 @JsName("createEngine")
 fun Engine(): Engine {
     return DefaultEngine(context = DefaultEngineProperties())
+}
+
+@DslMarker
+annotation class RulesScopeDsl
+
+@DslMarker
+annotation class RuleScopeDsl
+
+interface ExactRuleScope {
+    // infix fun property(name: String)
+    // infix fun operator(name: String)
+    // infix fun value(value: Any?)
+    infix fun String.should(operator: String): ConditionBuilder
+    infix fun String.shouldNot(operator: String): ConditionBuilder
+}
+
+interface Condition {
+    infix fun and(condition: () -> Condition): Condition
+    infix fun and(condition: Condition): Condition
+    // infix fun and(name: String): String
+}
+
+interface ConditionBuilder {
+    infix fun to(value: Any?): Condition
+    infix fun than(value: Any?): Condition
+}
+
+@RuleScopeDsl
+interface RuleScope {
+    fun all(init: RuleScope.() -> Unit)
+    fun any(init: RuleScope.() -> Unit)
+    // fun exact(init: ExactRuleScope.() -> Unit)
+    infix fun String.shouldBe(operator: String): ConditionBuilder
+    infix fun String.shouldNotBe(operator: String): ConditionBuilder
+}
+
+
+
+@RulesScopeDsl
+interface RulesScope : RuleScope {
+
+}
+
+@RulesScopeDsl
+interface EngineScope {
+    fun rules(init: RulesScope.() -> Unit)
+
+    fun operator(name: String)
+    fun rule(name: String, rule: Rule)
+    fun <T> fact(name: String, value: ValueProvider<T>)
+}
+
+fun runEngine(init: EngineScope.() -> Unit) {
+    val scope: EngineScope = TODO()
+    scope.init()
+
+}
+
+fun test() {
+    runEngine {
+        rules {
+            any {
+                all {
+                    let {
+                        "event.type" shouldBe "equal" to "temperature"
+                    } and {
+                        "event.payload.value" shouldNotBe "less" to 10
+                    }
+                }
+                all {
+
+                }
+            }
+        }
+    }
 }
